9a1bfefd2c4c253a7438d561a5a12ed3f0ff6997
marvin <marvin23m@gmail.com>
2020-11-23 13:29:09 +0000

    Add password and form validation before storing data

--- script.js
+++ script.js
@@ -5,20 +5,60 @@ const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm() {
     // Using Constraint API
     isValid = form.checkValidity();
     // Style main message for an error
-    message.textContent = 'Please fill out all fields.';
-    message.style.color = 'red';
-    messageContainer.style.borderColor = 'red';
+    if (!isValid) {
+        message.textContent = 'Please fill out all fields.';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        return;
+    }
+    // Check to see if passwords match
+    if (password1El.value === password2El.value) {
+        passwordsMatch = true;
+        password1El.style.borderColor = 'green';
+        password2El.style.borderColor = 'green';
+    } else {
+        passwordsMatch = false;
+        message.textContent = 'Make sure passwords match.';
+        message.style.color = 'red';
+        messageContainer.stye.borderColor = 'red';
+        password1El.style.borderColor = 'red';
+        password2El.style.borderColor = 'red';
+        return;
+    }
+    // if form is valid and passwords match
+    if (isValid && passwordsMatch) {
+        message.textContent = 'Successfully Registered!';
+        message.style.color = 'green';
+        messageContainer.style.borderColor = 'green';
+    }
+}
+
+function storeFormData() {
+    const user = {
+        name: form.name.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        website: form.website.value,
+        password: form.password.value
+    }
+
+    console.log(user);
 }
 
 function processFormData(e){
     e.preventDefault();
     // Validate Form
     validateForm();
+    // Submit Data if Valid
+    if (isValid && passwordsMatch) {
+        storeFormData();
+    }
 }
 
 // Event Listener
